bf6ceb7b6d6e4e6e2d4068e27ff2f2223548eda5
Brandonqr <brandonqr1@gmail.com>
2017-05-30 23:27:21 +0200

    Se ha instalado mongodb

--- dist/index.bundle.js
+++ dist/index.bundle.js
@@ -64,14 +64,50 @@ module.exports =
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ 	return __webpack_require__(__webpack_require__.s = 4);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-module.exports = require("express");
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+const devConfig = {
+  MONGO_URL: 'mongodb://localhost/quack-dev'
+};
+
+const testConfig = {
+  MONGO_URL: 'mongodb://localhost/quack-test'
+};
+
+const prodConfig = {
+  MONGO_URL: 'mongodb://localhost/quack-prod'
+};
+
+const defaultConfig = {
+  PORT: process.env.PORT || 3000
+};
+
+function envConfig(env) {
+  switch (env) {
+    case 'development':
+      return devConfig;
+    case 'test':
+      return testConfig;
+    default:
+      return prodConfig;
+
+  }
+}
+exports.default = _extends({}, defaultConfig, envConfig(process.env.NODE_ENV));
 
 /***/ }),
 /* 1 */
@@ -80,22 +116,116 @@ module.exports = require("express");
 "use strict";
 
 
-var _express = __webpack_require__(0);
+var _mongoose = __webpack_require__(8);
+
+var _mongoose2 = _interopRequireDefault(_mongoose);
+
+var _constants = __webpack_require__(0);
+
+var _constants2 = _interopRequireDefault(_constants);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+_mongoose2.default.Promise = global.Promise;
+
+try {
+  _mongoose2.default.connect(_constants2.default.MONGO_URL);
+} catch (e) {
+  mongodb.createConnection(_constants2.default.MONGO_URL);
+}
+
+_mongoose2.default.connection.once('open', () => console.log('MongoDB Ruunning')).on('error', e => {
+  throw e;
+});
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _morgan = __webpack_require__(9);
+
+var _morgan2 = _interopRequireDefault(_morgan);
+
+var _bodyParser = __webpack_require__(5);
+
+var _bodyParser2 = _interopRequireDefault(_bodyParser);
+
+var _compression = __webpack_require__(6);
+
+var _compression2 = _interopRequireDefault(_compression);
+
+var _helmet = __webpack_require__(7);
+
+var _helmet2 = _interopRequireDefault(_helmet);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = process.env.NODE_ENV === 'produuction';
+
+exports.default = app => {
+  if (isProd) {
+    app.use((0, _compression2.default)());
+    app.use((0, _helmet2.default)());
+  }
+
+  app.use(_bodyParser2.default.json());
+  app.use(_bodyParser2.default.urlencoded({ extended: true }));
+
+  if (isDev) {
+    app.use((0, _morgan2.default)('dev'));
+  }
+};
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports) {
+
+module.exports = require("express");
+
+/***/ }),
+/* 4 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _express = __webpack_require__(3);
 
 var _express2 = _interopRequireDefault(_express);
+
+var _constants = __webpack_require__(0);
+
+var _constants2 = _interopRequireDefault(_constants);
+
+__webpack_require__(1);
+
+var _middlewares = __webpack_require__(2);
+
+var _middlewares2 = _interopRequireDefault(_middlewares);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 const app = (0, _express2.default)();
+(0, _middlewares2.default)(app);
 
-const PORT = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+  res.send("hello word");
+});
 
-app.listen(PORT, err => {
+app.listen(_constants2.default.PORT, err => {
   if (err) {
     throw err;
   } else {
     console.log(`
-        Server running on port: ${PORT}
+        Server running on port: ${_constants2.default.PORT}
         ----
         Running on ${process.env.NODE_ENV}
         ---
@@ -103,6 +233,36 @@ app.listen(PORT, err => {
         `);
   }
 });
+
+/***/ }),
+/* 5 */
+/***/ (function(module, exports) {
+
+module.exports = require("body-parser");
+
+/***/ }),
+/* 6 */
+/***/ (function(module, exports) {
+
+module.exports = require("compression");
+
+/***/ }),
+/* 7 */
+/***/ (function(module, exports) {
+
+module.exports = require("helmet");
+
+/***/ }),
+/* 8 */
+/***/ (function(module, exports) {
+
+module.exports = require("mongoose");
+
+/***/ }),
+/* 9 */
+/***/ (function(module, exports) {
+
+module.exports = require("morgan");
 
 /***/ })
 /******/ ]);
--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import express from 'express';
+import constants from './config/constants';
+import './config/database';
+import middlewaresConfig from './config/middlewares'
 
 const app = express();
+middlewaresConfig(app);
 
-const PORT = process.env.PORT || 3000;
+app.get('/', (req, res)=>{
+  res.send("hello word");
+});
 
-app.listen(PORT, err => {
+
+app.listen(constants.PORT, err => {
   if (err) {
     throw err;
   }
     else {
       console.log(`
-        Server running on port: ${PORT}
+        Server running on port: ${constants.PORT}
         ----
         Running on ${process.env.NODE_ENV}
         ---
